16d329ae63f78c35a2e8df0a1f0ef4b63b4b0def
hieuthai <hieuthai642@gmail.com>
2020-07-18 14:10:14 +0700

    WIP react electron

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-docker-compose up -d --build
+#!/usr/bin/env bash
+xhost +local:root;
+docker network create fcs_network;
+docker-compose down && docker-compose build --no-cache && docker-comppse up -d;
